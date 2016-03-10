49bb2e06295d606492d18e56043b6237f88e22ea
stephens999 <stephens999@gmail.com>
2016-03-10 09:14:09 -0600

    improve tests of null loglik, in preparation for change in computation in code

--- tests/testthat/test_loglik.R
+++ tests/testthat/test_loglik.R
@@ -1,10 +1,18 @@
 test_that("calc_null_loglik gives expected answer under normal", {
-	set.seed(1); z=rnorm(100); expect_equal(sum(log(dnorm(z))),ashr::calc_null_loglik(z,1,NULL))
+	set.seed(1); z=rnorm(100); s = rgamma(100,10,10); 
+	alpha=0.5 
+	expect_equal(sum(-alpha*log(s)+dnorm(z/(s^alpha),log=TRUE, sd=s^(1-alpha))),ashr::calc_null_loglik(z,s,NULL,alpha=alpha));
+	expect_equal(sum(-log(s)+dnorm(z/s,log=TRUE, sd=1)),ashr::calc_null_loglik(z,s,NULL,alpha=alpha));
+	
+	alpha=0.1 
+	expect_equal(sum(-alpha*log(s)+dnorm(z/(s^alpha),log=TRUE, sd=s^(1-alpha))),ashr::calc_null_loglik(z,s,NULL,alpha=alpha))
 })
 
 
 test_that("calc_null_loglik gives expected answer under t", {
-        set.seed(1); z=rnorm(100); expect_equal(sum(log(dt(z,df=2))),ashr::calc_null_loglik(z,1,df=2))
+        set.seed(1); z=rnorm(100); s = rgamma(100,10,10);
+        alpha=0.5
+        expect_equal(sum(-log(s) + dt(z/s,df=2,log=TRUE)),ashr::calc_null_loglik(z,s,df=2,alpha=alpha))
 })
 
 test_that("calc_logLR is 0 for when g is null", {
