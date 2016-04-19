68ee715a00f09968755e790fb076b2088f47ebff
David Gerard <gerard.1787@gmail.com>
2016-04-19 15:21:52 -0500

    remove ashr:: from test_optmethod.R

--- tests/testthat/test_optmethod.R
+++ tests/testthat/test_optmethod.R
@@ -3,7 +3,7 @@ test_that("optmethod switches to EM when given negative probabilities", {
         betahat <- c(0.7775, 0.6091, 0.6880, 0.6897, 0.6565, 0.7505,
                      0.7125, 0.7201, 0.7498, 0.7553)
         sebetahat <- rep(0.01, length = length(betahat))
-        ash_out <- ashr::ash(betahat = betahat, sebetahat = sebetahat,
+        ash_out <- ash(betahat = betahat, sebetahat = sebetahat,
                              mixcompdist = "uniform", optmethod = "mixIP")
         expect_true(min(ash_out$fitted.g$pi) > -10 ^ -12)                 
 })
