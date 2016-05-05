e6b001fcfc41707daaf0753b9311b5eda22e4c22
stephens999 <stephens999@gmail.com>
2016-05-05 08:01:05 -0500

    update test for svalue

--- tests/testthat/test_old_vs_new.R
+++ tests/testthat/test_old_vs_new.R
@@ -1,7 +1,7 @@
 test_that("new results match old ones ", {
 # these results were saved under v1.1.3 before introducing more stable
 # calculation of likelihoods via first computing log-likelihood and normalizing
-#  set.seed(1); z=rnorm(100); z.ash=ash(z,1); saveRDS(z.ash,file="tests/testthat/z.ash.test")
+# set.seed(1); z=rnorm(100); z.ash=ash(z,1); saveRDS(z.ash,file="tests/testthat/z.ash.test")
   set.seed(1); z=rnorm(100); z.ash=ash(z,1);
   oldres = readRDS("z.ash.test")
   expect_equal(modifyList(oldres,list(optmethod=NULL)),
