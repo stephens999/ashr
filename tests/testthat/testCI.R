5866afae4411cd57bcce6b1e0b8c818b68e6fe53
stephens999 <stephens999@gmail.com>
2016-07-25 16:38:08 -0700

    add CI test

---
+++ tests/testthat/testCI.R
@@ -0,0 +1,18 @@
+test_that("CI result works for uniform prior", {
+  set.seed(17)
+  s = rgamma(100,10,10)
+  z = rnorm(100,0,s+1)
+  g = unimix(c(1),-1000,1000) #make prior very flat
+  a = ash(z,s,g=g,fixg=TRUE)
+  a2 = ash(z,s,df=4,g=g,fixg=TRUE)
+  a3 = ash(z,s,alpha=1,g=g,fixg=TRUE)
+ 
+  ci1=ashci(a,betaindex = 1:100)
+  expect_equal(ci1[,4],z-1.96*s,tol=0.01)
+ 
+  ci2 = ashci(a2,betaindex = 1:100)
+  expect_equal(ci2[,4],z-qt(0.975,df=4)*s,tol=0.01)
+
+  ci3 = ashci(a3,betaindex = 1:100)
+  expect_equal(ci3[,4],z-1.96*s,tol=0.01)
+})
