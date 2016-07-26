23a02011657bce0616bd39386d891094d9cb7962
stephens999 <stephens999@gmail.com>
2016-07-25 22:07:31 -0700

    simplify CI matrix computation; remove parallel

--- R/ashCI.R
+++ R/ashCI.R
@@ -1,41 +1,24 @@
 #' @title Credible Interval Computation for the ash object
 #'
-#' @description Given the ash object return by the main function ash,
-#'     this function computes the corresponding credible interval of
-#'     the mixture model.
+#' @description Given the ash object returned by the main function ash,
+#'     this function computes a posterior credible interval (CI) for each observation. The ash object
+#'     must include a data component to use this function (which it does by default).
 #'
-#' @details Uses default optimization function and perform
-#'     component-wise credible interval computation. The computation
-#'     cost is linear of the length of betahat.
+#' @details Uses uniroot to find credible interval, one at a time for each observation. 
+#' The computation cost is linear in number of observations.
 #'
 #' @param a the fitted ash object
 #' @param level the level for the credible interval, (default=0.95)
 #' @param betaindex a vector consisting of locations of betahat where
 #'     you would like to compute the credible interval
-#' @param lfsrcriteria a scalar, in which the function would
-#'     autoselect betahats based on lfsr value smaller than
-#'     lfsrcriteria when index is not supplied. Setting it to 1 would
-#'     compute credible interval for all observations.
-#' @param tol the desired accuracy, default value is 1e-5.
-#' @param maxcounts a positive integer used as the maximum iterations
-#'     to carry additional optimization on a refined interval,when you
-#'     see Actual cdf deviates from (1-level)/2,(1+level)/2 by too
-#'     much, try to increase this number. (default is 100)
-#' @param shrinkingcoefficient A positive real number smaller than 1
-#'     (default is 0.9), used to shrink to search interval for lower
-#'     and upper bound of the credible interval
+#' @param lfsr_threshold a scalar, if specified then computes CIs only for observations
+#' more significant than that threshold. 
+#' @param tol passed to uniroot; indicates desired accuracy.
 #' @param trace a logical variable denoting whether some of the
 #'     intermediate results of iterations should be displayed to the
 #'     user. Default is FALSE.
-#' @param ncores Whether to use parallel computing, defaults to FALSE,
-#'     user could specify number of cores they would like to
-#'     use. Further, if user does not specify and we have over 1000
-#'     entries to compute, then the function would perform parallel
-#'     computation using number of CPU cores on the current host.
-#' @return A matrix, with first column being the location,second
-#'     column being lfsr, 3rd column being posterior mean,4th and 5th
-#'     column being the lower bound and upper bound for the credible
-#'     interval.
+
+#' @return A matrix, with 2 columns, ith row giving CI for ith observation
 #'
 #'
 #' @export
@@ -46,32 +29,10 @@
 #' beta.ash = ash(betahat, sebetahat)
 #'
 #' CImatrix=ashci(beta.ash,betahat,sebetahat,level=0.95)
-#' print(CImatrix)
-#' print(CImatrix[order(CImatrix[,2]),]) # Sorted according to the lfsr
 #'
 #' CImatrix1=ashci(beta.ash,betahat,sebetahat,level=0.95,betaindex=c(1,2,5))
 #' CImatrix2=ashci(beta.ash,betahat,sebetahat,level=0.95,lfsrcriteria=0.1)
-#' #CImatrix3=ashci(beta.ash,betahat,sebetahat,level=0.95, betaindex=c(1:length(beta)),ncores=4)
-#' print(CImatrix1)
-#' print(CImatrix2)
-#' #print(CImatrix3)
-#'
-#' ##A larger example
-#' #beta = c(rep(0,1000),rnorm(1000))
-#' #sebetahat = abs(rnorm(2000,0,1))
-#' #betahat = rnorm(2000,beta,sebetahat)
-#' #beta.ash = ash(betahat, sebetahat)
-#' #CImatrix4 = ashci(beta.ash,betahat,sebetahat,level=0.95, betaindex=c(1:length(beta)),ncores=4)
-#todo/issue=> all set!
-#1.Q:Could do parallel computing to reduce the computation time
-#1.A:Done by doParallel
-#
-#2.Q:Optimization function does not work well for discountinous function, even
-#if it's a monotone one. Current remedy is to set a more conservative value for
-#the searching interval from the mixture
-#2.A:Done by shrinking searching interval using while loop
-#
-ashci = function (a,level=0.95,betaindex,lfsrcriteria=0.05,tol=1e-5, maxcounts=100,shrinkingcoefficient=0.9,trace=FALSE,ncores=FALSE){
+ashci = function (a,level=0.95,betaindex,lfsrcriteria=0.05,tol=1e-3, maxcounts=100,trace=FALSE){
   data = a$data
   if(is.null(data)){stop("ash object has to have data returned to compute CIs; use outputlevel 2 or more when running ash")}
  
@@ -101,110 +62,43 @@ ashci = function (a,level=0.95,betaindex,lfsrcriteria=0.05,tol=1e-5, maxcounts=1
       trace=TRUE  #component-wise computation takes more time
     }else {trace=FALSE}
   }
-  
-  if(missing(ncores)){
-    if(length(betaindex)>1000) ncores=detectCores()
-  }
-  if(ncores==FALSE){
-    ## Proceed with sequential computation
-    if(trace==TRUE){
+
+  if(trace==TRUE){
       cat("Computation time will be linear w.r.t sample size, progress will be printed to the screen \n")
       tic()
-    }
-    for(i in betaindex){
-      data_i = extract_data(data,i)
-      
-      if(is.nan(PosteriorSD[i])){
-        CImatrix[i,]=c(NA,NA)
-      } else if(PosteriorSD[i]==0){ #special case where posterior is (approximately) point mass
-        CImatrix[i,]=PosteriorMean[i]
-      } else {
-        #find lower point (first checking if 0 is it)
-        if(NegativeProb[i]<(1-level)/2 & (ZeroProb[i]+NegativeProb[i])> (1-level)/2){
-          CImatrix[i,1]=0;
-        } else {
-          CImatrix[i,1]=stats::uniroot(f=taildiff,interval=c(PosteriorMean[i]-2*PosteriorSD[i],PosteriorMean[i]),extendInt="upX",m=m,data=data_i,target=(1-level)/2,tol=tol)$root
-        }
-        
-        #find upper point (first checking if 0 is it)
-        if(PositiveProb[i] < ((1-level)/2) & (ZeroProb[i]+PositiveProb[i])> (1-level)/2){
-          CImatrix[i,2]=0;
-        } else {
-          CImatrix[i,2]=stats::uniroot(f=taildiff,interval=c(PosteriorMean[i],PosteriorMean[i]+2*PosteriorSD[i]),extendInt="upX",m=m,data=data_i,target=(1+level)/2,tol=tol)$root
-        }
-      }
-      if(trace==TRUE & percentage <=100){
-        currentpercentage=round(i*100/length(betaindex))
-        if(currentpercentage == percentage){
-          cat("Current computation progress", percentage,"%, seconds ")
-          toc()
-          percentage = percentage + 1}
-      }
-    }
-    CImatrix = CImatrix * data$s_orig^data$alpha #correct CIs for the fact they are CIs for beta/s^alpha
-  } else{
-    stop("not yet implemented in this branch")
-    ## Proceed with parallel computation
-    #if(trace==TRUE){
-    #cat("Computation time would be linear w.r.t sample size, parallel computation progress would be printed to the screen \n")
-    #tic()
-    #}
-    percentagevector=rep(1,ncores)
-    cl <- makePSOCKcluster(ncores)#This number corresponding to number of workers
-    registerDoParallel(cl)
-    CImatrix[,4:7]=foreach(j=1:length(betaindex), .combine='rbind') %dopar% {
-      i = betaindex[j]
-      lower = PosteriorMean[i]-PosteriorSD[i]
-      data_i = extract_data(data,i)
-      while(cdf_post(m,lower,data_i) > (1-level)/2){
-        lower = lower-PosteriorSD[i]
-      }
-      upper = PosteriorMean[i]+PosteriorSD[i]
-      while(cdf_post(m,upper,data_i) < 1 - (1-level)/2){
-        upper = upper+PosteriorSD[i]
-      }
-      
-      
-      #Calculating the lower bound
-      CIentryl=stats::optimize(f=ci.lower,interval=c(lower,PosteriorMean[i]),m=m,data=data_i,level=level,tol=tol)$minimum
-      cdfl=cdf_post(m, CIentryl,data_i)
-      #If the actual cdf deviates by too much, refine the optimization search interval
-      #currently we set maximum value of execution to maxcounts to avoid dead loop
-      counts=0
-      intervallength=PosteriorMean[i]-lower
-      while(abs(cdfl-(1-level)/2)>(10*tol) & counts<maxcounts){
-        intervallength= intervallength*shrinkingcoefficient
-        CIentryl=stats::optimize(f=ci.lower,interval=c(PosteriorMean[i]-intervallength,
-                                                       PosteriorMean[i]),m=m,data=data_i,level=level, tol=tol)$minimum
-        cdfl=cdf_post(m, CIentryl,data_i)
-        counts=counts+1
-      }
-      
-      #Calculating the upper bound
-      CIentryu=stats::optimize(f=ci.upper,interval=c(PosteriorMean[i],upper),m=m,data=data_i,level=level,
-                               tol=tol)$minimum
-      cdfu=cdf_post(m, CIentryu,data_i)
-      #If the actual cdf deviates by too much, refine the optimization search interval
-      #currently we set maximum value of execution to maxcounts to avoid dead loop
-      counts=0
-      intervallength=upper-PosteriorMean[i]
-      while(abs(cdfu-(1+level)/2)>(10*tol) & counts<maxcounts){
-        intervallength= intervallength*shrinkingcoefficient
-        CIentryu=stats::optimize(f=ci.upper,interval=c(PosteriorMean[i],PosteriorMean[i]+intervallength),
-                                 m=m,data=data_i,level=level,
-                                 tol=tol)$minimum
-        cdfu=cdf_post(m, CIentryu,data_i)
-        counts=counts+1
-      }
-      
-      #sending the result back to master
-      c(CIentryl, CIentryu,cdfl,cdfu)
-    }
-    stopCluster(cl)
   }
-
-  #CImatrix[i,6]=pcdf_post(m,CImatrix[,1],data)
-  #CImatrix[i,7]=pcdf_post(m,CImatrix[,2],data)
+  for(i in betaindex){
+    data_i = extract_data(data,i)
+    
+    if(is.nan(PosteriorSD[i])){
+      CImatrix[i,]=c(NA,NA)
+    } else if(PosteriorSD[i]==0){ #special case where posterior is (approximately) point mass
+      CImatrix[i,]=PosteriorMean[i]
+    } else {
+      #find lower point (first checking if 0 is it)
+      if(NegativeProb[i]<(1-level)/2 & (ZeroProb[i]+NegativeProb[i])> (1-level)/2){
+        CImatrix[i,1]=0;
+      } else {
+        CImatrix[i,1]=stats::uniroot(f=taildiff,interval=c(PosteriorMean[i]-2*PosteriorSD[i],PosteriorMean[i]),extendInt="upX",m=m,data=data_i,target=(1-level)/2,tol=tol)$root
+      }
+      
+      #find upper point (first checking if 0 is it)
+      if(PositiveProb[i] < ((1-level)/2) & (ZeroProb[i]+PositiveProb[i])> (1-level)/2){
+        CImatrix[i,2]=0;
+      } else {
+        CImatrix[i,2]=stats::uniroot(f=taildiff,interval=c(PosteriorMean[i],PosteriorMean[i]+2*PosteriorSD[i]),extendInt="upX",m=m,data=data_i,target=(1+level)/2,tol=tol)$root
+      }
+    }
+    if(trace==TRUE & percentage <=100){
+      currentpercentage=round(i*100/length(betaindex))
+      if(currentpercentage == percentage){
+        cat("Current computation progress", percentage,"%, seconds ")
+        toc()
+        percentage = percentage + 1}
+    }
+  }
+  CImatrix = CImatrix * data$s_orig^data$alpha #correct CIs for the fact they are CIs for beta/s^alpha
+  
   CImatrix=signif(CImatrix,digits=round(1-log(tol)/log(10)))
   return(CImatrix)
 }
