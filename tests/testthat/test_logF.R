ed4286d5aaa391773c68579a98e667737e1263ea
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-12-15 00:43:36 -0600

    Made a few improvements to test_logF.R: (1) Added detailed explanation of steps in comments; (2) improved code spacing and variable nodes; (3) fixed bug in which $PosteriorMean returned NULL.

--- tests/testthat/test_logF.R
+++ tests/testthat/test_logF.R
@@ -1,8 +1,23 @@
-test_that("logF error with df=10,10 gives similar answers to normal error", {
-  set.seed(10)
-  e = rnorm(100)+log(rf(100,df1=10,df2=10))
-  e.ash = ash(e,1,lik=logF_lik(df1=10,df2=10))
-  s = sd(log(rf(10000,10,10))) # get approximate standard deviation by simulation
-  e.ash.norm = ash(e,s)
-  expect_equal(e.ash.norm$PosteriorMean,e.ash$PosteriorMean,tol=0.05)
+test_that("logF error with df=(10,10) gives similar answers to normal error",{
+
+  # Simulate a data set.
+  set.seed(1)
+  x <- rnorm(100) + log(rf(100,df1 = 10,df2 = 10))
+
+  # Fit the ash model with two different likelihood densities: (1) the
+  # normal distribution, and (2) the log-F distribution with degrees
+  # of freedom (10,10). In the first case, we set the standard errors
+  # (s.e.) to be match the empirical standard deviation of random
+  # draws from the log-F distribution.
+  s            <- sd(log(rf(10000,df1 = 10,df2 = 10)))
+  ash.norm.out <- ash(x,s)
+  ash.logF.out <- ash(x,1,lik = logF_lik(df1 = 10,df2 = 10))
+
+  # Compare the posterior mean estimates from ash using the two
+  # different likelihood densities. We expect that the difference
+  # between the two estimates should always be small (relative error
+  # at most 5%).
+  expect_equal(ash.norm.out$result$PosteriorMean,
+               ash.logF.out$result$PosteriorMean,
+               tolerance = 0.05)
 })
