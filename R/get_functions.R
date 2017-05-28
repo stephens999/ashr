57c36b725e13ddf45de318f079c03c28ee343a13
stephens999 <stephens999@gmail.com>
2017-05-28 13:11:32 -0500

    make get_lfsr a generic

--- R/get_functions.R
+++ R/get_functions.R
@@ -1,4 +1,20 @@
-#' @title Return lfsr, lfdr, etc from ash object
+#' Return local false sign rate from ash or mash fit
+#' @param x an ash or mash fit (e.g. from running ash or mash)
+#' @return a vector (ash) or matrix (mash) of local false sign rates
+#' @export
+get_lfsr = function(x){
+  UseMethod("get_lfsr")
+}
+#' @export
+get_lfsr.default = function(x){
+  stop(paste("Invalid class", class(x), "for first argument in",  match.call()))
+}
+
+#' @title Return lfsr from an ash object
+#' @export
+get_lfsr.ash=function(x){x$result$lfsr}
+
+#' @title Return lfdr, etc from ash object
 #'
 #' @description These functions simply return elements of an ash object, generally without doing any calculations. 
 #' (So if the value was not computed during the original call to ash, eg because of how outputlevel was set in the call, 
@@ -7,54 +23,48 @@
 #' rather than directly from the ash object will help ensure compatability moving forward
 #' (e.g. if the internal structure of the ash object changes during software development.)
 #'
-#' @param a the fitted ash object
-#'
-#' @describeIn get_lfsr local false sign rate
-#' @export
-get_lfsr=function(a){a$result$lfsr}
-
-#' @describeIn get_lfsr local false discovery rate
+#' @describeIn get_lfdr local false discovery rate
 #' @export
 get_lfdr=function(a){a$result$lfdr}
 
-#' @describeIn get_lfsr svalue
+#' @describeIn get_lfdr svalue
 #' @export
 get_svalue=function(a){a$result$svalue}
 
-#' @describeIn get_lfsr qvalue
+#' @describeIn get_lfdr qvalue
 #' @export
 get_qvalue=function(a){a$result$qvalue}
 
-#' @describeIn get_lfsr posterior mean
+#' @describeIn get_lfdr posterior mean
 #' @export
 get_pm=function(a){a$result$PosteriorMean}
 
-#' @describeIn get_lfsr posterior standard deviation
+#' @describeIn get_lfdr posterior standard deviation
 #' @export
 get_psd=function(a){a$result$PosteriorSD}
 
-#' @describeIn get_lfsr positive probability
+#' @describeIn get_lfdr positive probability
 #' @export
 get_pp=function(a){a$result$PositiveProb}
 
-#' @describeIn get_lfsr negative probability
+#' @describeIn get_lfdr negative probability
 #' @export
 get_np=function(a){a$result$NegativeProb}
 
-#' @describeIn get_lfsr log-likelihood 
+#' @describeIn get_lfdr log-likelihood 
 #' @export
 get_loglik=function(a){a$loglik}
 
-#' @describeIn get_lfsr log-likelihood ratio
+#' @describeIn get_lfdr log-likelihood ratio
 #' @export
 get_logLR=function(a){a$logLR}
 
-#' @describeIn get_lfsr fitted g mixture
+#' @describeIn get_lfdr fitted g mixture
 #' @export
 get_fitted_g=function(a){a$fitted_g}
 
 
-#' @describeIn get_lfsr pi0, the proportion of nulls 
+#' @describeIn get_lfdr pi0, the proportion of nulls 
 #' @export
 get_pi0 = function(a){
   null.comp = comp_sd(a$fitted_g)==0
