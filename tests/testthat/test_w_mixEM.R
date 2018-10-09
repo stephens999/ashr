7147a88b4c861325f1a1579bc3ff343330190536
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-10-08 22:57:17 -0500

    The mix-SQP algorithm is now better integrated ashr; bumped up version.

--- tests/testthat/test_w_mixEM.R
+++ tests/testthat/test_w_mixEM.R
@@ -1,10 +1,14 @@
 test_that("optimization with weights matches expectations", {
   set.seed(1); z=rnorm(100,0,2) 
   z.ash= ash(z[1:50],1,optmethod="mixEM")
-  z.ash.w = ash(z,1,optmethod="w_mixEM",weights = c(rep(1,50),rep(0,50)), g=get_fitted_g(z.ash))
+  z.ash.w = ash(z,1,optmethod="w_mixEM",weights = c(rep(1,50),rep(0,50)),
+                g=get_fitted_g(z.ash))
   expect_equal(get_fitted_g(z.ash.w)$pi, get_fitted_g(z.ash)$pi, tol=0.001)
+
+  testthat::skip_if_not_installed("mixsqp")
   
   testthat::skip_if_not_installed("REBayes")
-  z.ash.w2 = ash(z,1,optmethod="mixIP",weights = c(rep(1,50),rep(0,50)), g=get_fitted_g(z.ash))
-  expect_equal(get_fitted_g(z.ash.w2)$pi, get_fitted_g(z.ash)$pi, tol=0.001)
+  z.ash.w3 = ash(z,1,optmethod="mixIP",weights = c(rep(1,50),rep(0,50)),
+                 g = get_fitted_g(z.ash))
+  expect_equal(get_fitted_g(z.ash.w3)$pi, get_fitted_g(z.ash)$pi, tol=0.001)
 })
