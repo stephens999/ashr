9570be6fe399594c06da928d9136b4393efa60c6
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-02-07 11:16:22 -0600

    Added more systematic checks for proper installation of REBayes & Rmosek in the testthat tests.

--- tests/testthat/test_w_mixEM.R
+++ tests/testthat/test_w_mixEM.R
@@ -12,8 +12,14 @@ test_that("optimization with weights matches expectations", {
   z.ash.w2 = ash(z,1,optmethod="mixSQP",weights = c(rep(1,50),rep(0,50)),
                  g = get_fitted_g(z.ash))
   expect_equal(get_fitted_g(z.ash.w2)$pi, get_fitted_g(z.ash)$pi, tol=1e-5)
-  
+
+  # Since the Rmosek package on CRAN will not work with REBayes, here
+  # I check whether the correct Rmosek package (the one downloaded
+  # from mosek.com) is installed.
   testthat::skip_if_not_installed("REBayes")
+  testthat::skip_if_not_installed("Rmosek")
+  testthat::skip_if(is.element("mosek_attachbuilder",
+                               getNamespaceExports("Rmosek")))
   z.ash.w3 = ash(z,1,optmethod="mixIP",weights = c(rep(1,50),rep(0,50)),
                  g = get_fitted_g(z.ash))
   expect_equal(get_fitted_g(z.ash.w3)$pi, get_fitted_g(z.ash)$pi, tol=1e-5)
