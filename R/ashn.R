f399fc297d1aeb1230de990d4ef8e1a62262baf7
Jason Willwerscheid <willwerscheid@uchicago.edu>
2019-10-05 18:05:17 -0400

    ash.estmode tol should depend on search range

--- R/ashn.R
+++ R/ashn.R
@@ -1,10 +1,11 @@
-# a wrapper function that estimates the mode, using optim
+# a wrapper function that estimates the mode, using optimize
 # called by ash if mode="estimate"
-ash.estmode = function(betahat,modemin,modemax,...){
-  test.op = function(c){return(-ash(betahat=betahat,mode=c,outputlevel="loglik",...)$loglik)}
-  #opt = stats::optimize(test.op,interval=c(min(betahat),max(betahat)))
-  opt = stats::optimize(test.op,interval=c(modemin,modemax))
+ash.estmode = function(betahat, modemin, modemax, ...){
+  opt.fn = function(c) {
+    return(-ash(betahat = betahat, mode = c, outputlevel = "loglik", ...)$loglik)
+  }
+  opt = stats::optimize(opt.fn, 
+                        interval = c(modemin, modemax),
+                        tol = (modemax - modemin) * .Machine$double.eps^0.25)
   return(opt$minimum)
 }
-
-
