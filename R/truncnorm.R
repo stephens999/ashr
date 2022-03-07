83fa8151df23c473264755d09faa15e760d6aef8
Sue Parkinson <suzannastep@gmail.com>
2022-03-07 14:46:13 -0600

    draft of vectorized function

--- R/truncnorm.R
+++ R/truncnorm.R
@@ -193,10 +193,125 @@ my_e2truncnorm = function(a, b, mean = 0, sd = 1) {
             # ⟹ if mean < a, 2nd moment is a^2
             return(a^2)
         } else if (mean > b){
-            # ⟹ if mean > a, 2nd moment is b^2
+            # ⟹ if mean > b, 2nd moment is b^2
             return(b^2)
         }
     }
+}
+
+#' @title Expected Squared Value of Truncated Normal
+#' 
+#' @description Computes the expected squared values of truncated normal 
+#'   distributions with parameters \code{a}, \code{b}, \code{mean}, and 
+#'   \code{sd}. Arguments can be scalars, vectors, or matrices. Arguments of 
+#'   shorter length will be recycled according to the usual recycling rules, 
+#'   but \code{a} and \code{b} must have the same length. Missing values are 
+#'   accepted for all arguments.
+#'
+#' @inheritParams my_etruncnorm
+#' 
+#' @param sd The standard deviation of the untruncated normal. Standard
+#'   deviations of zero are interpreted as numerically (rather than exactly)
+#'   zero, so that the square of the untruncated mean is returned if it lies 
+#'   within \code{[a, b]} and the square of the nearer of \code{a} and 
+#'   \code{b} is returned otherwise.
+#' 
+#' @return The expected squared values of truncated normal
+#'   distributions with parameters \code{a}, \code{b}, \code{mean}, and
+#'   \code{sd}. If any of the arguments is a matrix, then a matrix will
+#'   be returned.
+#'  
+#' @seealso \code{\link{my_etruncnorm}}, \code{\link{my_vtruncnorm}}
+#'     
+#' @export
+#'
+library(RcppFaddeeva)
+my_e2truncnormVECTORIZED = function(a, b, mean = 0, sd = 1) {
+    # based off of https://github.com/cossio/TruncatedNormal.jl/blob/3c16866c3afa3920e787513d492689e9e81192ca/src/tnmom2.jl
+    do_truncnorm_argchecks(a, b)
+
+    # initialize array to store result of shape like a
+    if (is.matrix(a))
+        res = array(dim = dim(a))
+    else
+        res = rep(NA, length.out = length(a))
+
+    #make sure input sizes all match 
+    a = rep(a, length.out = length(res))
+    b = rep(b, length.out = length(res))
+    mean = rep(mean, length.out = length(res))
+    sd = rep(sd, length.out = length(res))
+
+    # Handle zero sd point masses
+    sd.zero = (sd == 0)
+    # if mean ≥ b, 2nd moment is b^2
+    res[sd.zero & b <= mean] = b[sd.zero & b <= mean]^2
+    # if mean ≤ a, 2nd moment is a^2
+    res[sd.zero & a >= mean] = a[sd.zero & a >= mean]^2
+    # if mean ∈ (a,b), 2nd moment is mean^2
+    res[sd.zero & a < mean & mean < b] = mean[sd.zero & a < mean & mean < b]^2
+
+    # Rescale to standard normal distributions if sd is nonzero
+    alpha = (a[!sd.zero] - mean[!sd.zero]) / sd[!sd.zero]
+    beta = (b[!sd.zero] - mean[!sd.zero]) / sd[!sd.zero]
+    scaled.mean = my_etruncnorm(alpha, beta)
+    # initialize array for scaled 2nd moments
+    scaled.2mom = rep(NA, length.out = length(alpha))
+
+    # point mass bc endpoints are equal
+    # 2nd moment is point-value squared
+    endpoints_equal = (alpha == beta)
+    scaled.2mom[endpoints_equal] = alpha[endpoints_equal] ^ 2
+    # keep track of which spots in scaled.2mom are already computed
+    computed = endpoints_equal
+
+    # force to satisfy β ≥ 0 and |α| ≤ |β|
+    # so either α ≤ 0 ≤ β or 0 < α ≤ β
+    flip = !computed & abs(alpha) > abs(beta)
+    flip[is.na(flip)] = FALSE
+    orig.alpha = alpha
+    alpha[flip] = -beta[flip]
+    beta[flip] = -orig.alpha[flip]
+
+    # both endpoints infinite/untruncated normal distribution
+    # 2nd moment is 1
+    both_inf = !computed & is.infinite(alpha) & is.infinite(beta)
+    scaled.2mom[both_inf] = 1
+    computed = computed | both_inf
+
+    # truncated to [α,∞) 
+    # 2nd moment simplifies to 1 + αϕ(α)/(1 - Φ(α))
+    beta_inf = !computed & is.infinite(beta)
+    scaled.2mom[beta_inf] = 1 + sqrt(2 / pi) * alpha[beta_inf] / Re(erfcx(alpha[beta_inf] / sqrt(2)))
+    computed = computed | beta_inf
+
+    # a ≤ 0 ≤ b
+    #catestrophic cancellation is less of an issue
+    alpha_negative = !computed & alpha <= 0
+    ea = sqrt(pi/2) * Re(erf(alpha[alpha_negative] / sqrt(2)))
+    eb = sqrt(pi/2) * Re(erf(beta[alpha_negative] / sqrt(2)))
+    fa = ea - alpha[alpha_negative] * exp(-alpha[alpha_negative]^2 / 2)
+    fb = eb - beta[alpha_negative] * exp(-beta[alpha_negative]^2 / 2)
+    scaled.2mom[alpha_negative] = (fb - fa) / (eb - ea)
+    computed = computed | alpha_negative
+    
+    # 0 < a ≤ b
+    #strategically avoid catestrophic cancellation as much as possible
+    exdiff = exp((alpha[!computed] - beta[!computed])*(alpha[!computed] + beta[!computed])/2)
+    ea = sqrt(pi/2) * Re(erfcx(alpha[!computed] / sqrt(2)))
+    eb = sqrt(pi/2) * Re(erfcx(beta[!computed] / sqrt(2)))
+    fa = ea + alpha[!computed]
+    fb = eb + beta[!computed]
+    scaled.2mom[!computed] = (fa - fb * exdiff) / (ea - eb * exdiff)
+
+    # transform results back to nonstandard normal case
+    # μ(μ + 2σ * tn_mean(α, β)) + σ^2 tn_2nd_mom(α, β)
+    # TODO potential for catestrophic cancellation here... is there a better way?
+    # multiplying by mean outside is definitely better, but maybe an even better way?
+    res[!sd.zero] = mean*(mean + 2 * sd * scaled.mean)
+    res[!sd.zero] = res[!sd.zero] + sd^2 * scaled.2mom
+
+    return(res)
 }
 
 #' @title Variance of Truncated Normal
