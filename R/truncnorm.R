a8e3f1ac171fd2b90efc5eebc6d72fdf647b9567
Sue Parkinson <suzannastep@gmail.com>
2022-03-24 18:09:12 -0500

    add changes to other truncnorm functions

--- R/truncnorm.R
+++ R/truncnorm.R
@@ -29,65 +29,90 @@
 #' @seealso \code{\link{my_e2truncnorm}}, \code{\link{my_vtruncnorm}}
 #'   
 #' @export
-#' 
 my_etruncnorm = function(a, b, mean = 0, sd = 1) {
+  # based off of https://github.com/cossio/TruncatedNormal.jl
   do_truncnorm_argchecks(a, b)
   
-  # The case where some sds are zero is handled last. In the meantime, assume
-  #   that sd > 0.
+  # initialize array to store result of shape like a
+  if (is.matrix(a)){
+    res = array(dim = dim(a))
+  }
+  else{
+    res = rep(NA, length.out = length(a))
+  }
+    
+  #make sure input sizes all match 
+  a = rep(a, length.out = length(res))
+  b = rep(b, length.out = length(res))
+  mean = rep(mean, length.out = length(res))
+  sd = rep(sd, length.out = length(res))
+  
+  # Handle zero sds. Return the mean of the untruncated normal when it is 
+  #   located inside of the interval [alpha, beta]. Otherwise, return the 
+  #   endpoint that is closer to the mean.
+  sd.zero = (sd == 0)
+  res[sd.zero & b <= mean] = b[sd.zero & b <= mean]
+  res[sd.zero & a >= mean] = a[sd.zero & a >= mean]
+  res[sd.zero & a < mean & b > mean] = mean[sd.zero & a < mean & b > mean]
+  
+  # Focus in on where sd is nonzero
+  a = a[!sd.zero]
+  b = b[!sd.zero]
+  mean = mean[!sd.zero]
+  sd = sd[!sd.zero]
+  
+  # Rescale to standard normal distributions
   alpha = (a - mean) / sd
   beta = (b - mean) / sd
+  # initialize array for scaled 2nd moments
+  scaled.mean = rep(NA, length.out = length(alpha))
   
-  # Flip alpha and beta when: 1. Both are positive (since computations are 
-  #   unstable when both values of pnorm are close to 1); 2. dnorm(alpha) is 
-  #   greater than dnorm(beta) (since subtraction is done on the log scale).
-  flip = (alpha > 0 & beta > 0) | (beta > abs(alpha))
+  # point mass bc endpoints are equal
+  # 1st moment is point-value
+  endpoints_equal = (alpha == beta)
+  scaled.mean[endpoints_equal] = alpha[endpoints_equal]
+  # keep track of which spots in scaled.2mom are already computed
+  computed = endpoints_equal
+  
+  # force to satisfy β ≥ 0 and |α| ≤ |β|
+  # so either α ≤ 0 ≤ β or 0 < α ≤ β
+  flip = !computed & abs(alpha) > abs(beta)
   flip[is.na(flip)] = FALSE
   orig.alpha = alpha
   alpha[flip] = -beta[flip]
   beta[flip] = -orig.alpha[flip]
   
-  dnorm.diff = logscale_sub(dnorm(beta, log = TRUE), dnorm(alpha, log = TRUE))
-  pnorm.diff = logscale_sub(pnorm(beta, log.p = TRUE), pnorm(alpha, log.p = TRUE))
-  scaled.res = -exp(dnorm.diff - pnorm.diff)
+  # both endpoints infinite/untruncated normal distribution
+  # scaled mean is 0
+  both_inf = !computed & is.infinite(alpha) & is.infinite(beta)
+  scaled.mean[both_inf] = 0
+  computed = computed | both_inf
   
-  # Handle the division by zero that occurs when pnorm.diff = -Inf (that is, 
-  #   when endpoints are approximately equal).
-  endpts.equal = is.infinite(pnorm.diff)
-  scaled.res[endpts.equal] = (alpha[endpts.equal] + beta[endpts.equal]) / 2
+  # a ≤ 0 ≤ b
+  #catestrophic cancellation is less of an issue
+  alpha_negative = !computed & alpha <= 0
+  diff = (alpha[alpha_negative] - beta[alpha_negative]) * (alpha[alpha_negative] + beta[alpha_negative]) / 2
+  #√(2/π) * expm1(-Δ) * exp(-α^2 / 2) / erf(β/√2, α/√2)
+  scaled.mean[alpha_negative] = sqrt(2/pi) * expm1(-diff) * exp(-alpha[alpha_negative]^2 / 2) 
+  denom = Re(erf(alpha[alpha_negative]/√2)) - Re(erf(beta[alpha_negative]/√2))
+  scaled.mean[alpha_negative] = scaled.mean[alpha_negative] / denom
+  computed = computed | alpha_negative
   
-  # When alpha and beta are very large and both negative (due to the flipping
-  #   logic, they cannot both be positive), computations can become unstable. 
-  #   We find such cases by checking that the expectations make sense. When
-  #   beta is negative, beta + 1 / beta is a lower bound for the expectation.
-  #   Further, it is an increasingly good approximation as beta goes to -Inf 
-  #   as long as alpha and beta aren't too close to one another. If they are,
-  #   then their midpoint can be used as an alternate approximation (and lower 
-  #   bound).
-  lower.bd = pmax(beta + 1 / beta, (alpha + beta) / 2)
-  bad.idx = (!is.na(beta) & beta < 0 
-             & (scaled.res < lower.bd | scaled.res > beta))
-  scaled.res[bad.idx] = lower.bd[bad.idx]
+  # 0 < a < b
+  #strategically avoid catestrophic cancellation as much as possible
+  diff = (alpha[!computed] - beta[!computed]) * (alpha[!computed] + beta[!computed]) / 2
+  denom = exp(-diff) * Re(erfcx(beta[!computed] / sqrt(2))) - Re(erfcx(alpha[!computed] / sqrt(2)))
+  scaled.mean[!computed] = sqrt(2/pi) * expm1(-diff) / denom
+  
+  #double check that things are within bounds
+  scaled.mean[scaled.mean > beta] = beta[scaled.mean > beta]
+  scaled.mean[scaled.mean < alpha] = alpha[scaled.mean < alpha]
   
   # Flip back.
   scaled.res[flip] = -scaled.res[flip]
   
-  res = mean + sd * scaled.res
-  
-  # Handle zero sds. Return the mean of the untruncated normal when it is 
-  #   located inside of the interval [alpha, beta]. Otherwise, return the 
-  #   endpoint that is closer to the mean.
-  if (any(sd == 0)) {
-    # For the subsetting to work correctly, arguments need to be recycled.
-    a = rep(a, length.out = length(res))
-    b = rep(b, length.out = length(res))
-    mean = rep(mean, length.out = length(res))
-    
-    sd.zero = (sd == 0)
-    res[sd.zero & b <= mean] = b[sd.zero & b <= mean]
-    res[sd.zero & a >= mean] = a[sd.zero & a >= mean]
-    res[sd.zero & a < mean & b > mean] = mean[sd.zero & a < mean & b > mean]
-  }
+  #transform back to nonstandard normal case
+  res[!sd.zero] = mean + sd * scaled.res
   
   return(res)
 }
@@ -120,14 +145,16 @@ my_etruncnorm = function(a, b, mean = 0, sd = 1) {
 #'
 library(RcppFaddeeva)
 my_e2truncnorm = function(a, b, mean = 0, sd = 1) {
-  # based off of https://github.com/cossio/TruncatedNormal.jl/blob/3c16866c3afa3920e787513d492689e9e81192ca/src/tnmom2.jl
+  # based off of https://github.com/cossio/TruncatedNormal.jl
   do_truncnorm_argchecks(a, b)
 
   # initialize array to store result of shape like a
-  if (is.matrix(a))
+  if (is.matrix(a)){
     res = array(dim = dim(a))
-  else
+  }
+  else{
     res = rep(NA, length.out = length(a))
+  }
 
   #make sure input sizes all match 
   a = rep(a, length.out = length(res))
@@ -204,25 +231,16 @@ my_e2truncnorm = function(a, b, mean = 0, sd = 1) {
   scaled.2mom[!computed] = (fa - fb * exdiff) / (ea - eb * exdiff)
 
   # transform results back to nonstandard normal case
-  # μ(μ + 2σ * tn_mean(α, β)) + σ^2 tn_2nd_mom(α, β)
-  # TODO potential for catestrophic cancellation here... is there a better way?
-  # multiplying by mean outside is definitely better, but maybe an even better way?
-  res[!sd.zero] = mean*(mean + 2 * sd * scaled.mean)
-  res[!sd.zero] = res[!sd.zero] + sd^2 * scaled.2mom
+  # μ^2 + σ^2 E(Z^2) + 2 μ σ E(Z)
+  # possible catestropic cancellation because μ^2 + σ^2 E(Z^2) ≧ 0 while 2μσE(Z) has unknown sign
+  # If |μ| < σ , compute as μ^2 + σ(σ E(Z^2) + 2 μ E(Z))
+  mean_smaller = abs(mean) < sd
+  res[!sd.zero & mean_smaller] = mean^2 + sd*(sd * scaled.2mom + 2 * mean * scaled.mean)
+  # If  σ ≦ |μ| , compute as μ(μ +  2 σ E(Z)) + σ^2 E(Z^2)
+  res[!sd.zero & !mean_smaller] = mean*(mean + 2 * sd * scaled.mean) + sd^2 * scaled.2mom
 
-  # TODO ADD IN ERROR CHECKING
-  # Check that the results make sense. When beta is negative,
-  # #   beta^2 + 2 * (1 + 1 / beta^2) is an upper bound for the expected squared
-  # #   value, and it is typically a good approximation as beta goes to -Inf. 
-  # #   When the endpoints are very close to one another, the expected squared 
-  # #   value of the uniform distribution on [alpha, beta] is a better upper 
-  # #   bound (and approximation).
-  # upper.bd1 = beta^2 + 2 * (1 + 1 / beta^2)
-  # upper.bd2 = (alpha^2 + alpha * beta + beta^2) / 3
-  # upper.bd = pmin(upper.bd1, upper.bd2)
-  # bad.idx = (!is.na(beta) & beta < 0 
-  #            & (scaled.res < beta^2 | scaled.res > upper.bd))
-  # scaled.res[bad.idx] = upper.bd[bad.idx]
+  # Check that the results make sense -- should never be negative
+  stopifnot(all(res >= 0))  
 
   return(res)
 }
@@ -244,30 +262,26 @@ my_e2truncnorm = function(a, b, mean = 0, sd = 1) {
 #' @seealso \code{\link{my_etruncnorm}}, \code{\link{my_e2truncnorm}}
 #'
 #' @export
-#' 
 my_vtruncnorm = function(a, b, mean = 0, sd = 1) {
+  # based off of https://github.com/cossio/TruncatedNormal.jl
   do_truncnorm_argchecks(a, b)
   
+  #solved scaled problem
   alpha = (a - mean) / sd
   beta = (b - mean) / sd
   
-  scaled.res = my_e2truncnorm(alpha, beta) - my_etruncnorm(alpha, beta)^2
+  m1 = my_etruncnorm(alpha, beta)
+  m2 = sqrt(my_e2truncnorm(alpha, beta))
+  scaled.res = (m2 - m1) * (m1 + m2)
   
-  # When alpha and beta are large and share the same sign, this computation 
-  #   becomes unstable. A good approximation in this regime is 1 / beta^2
-  #   (when alpha and beta are both negative). If my_e2truncnorm and
-  #   my_etruncnorm are accurate to the eighth digit, then we can only trust 
-  #   results up to the second digit if beta^2 and 1 / beta^2 differ by an
-  #   order of magnitude no more than 6.
-  smaller.endpt = pmin(abs(alpha), abs(beta))
-  bad.idx = (is.finite(smaller.endpt) & smaller.endpt > 30)
-  scaled.res[bad.idx] = pmin(1 / smaller.endpt[bad.idx]^2,
-                             (beta[bad.idx] - alpha[bad.idx])^2 / 12)
+  # Handle endpoints equal
+  scaled.res[alpha == beta] = alpha[alpha == beta]
+  
+  #transform back to unscaled
+  res = sd^2 * scaled.res
   
   # Handle zero sds.
-  scaled.res[is.nan(scaled.res)] = 0
-  
-  res = sd^2 * scaled.res
+  res[sd == 0] = 0
   
   return(res)
 }
@@ -277,21 +291,4 @@ do_truncnorm_argchecks = function(a, b) {
     stop("truncnorm functions require that a and b have the same length.")
   if (any(b < a, na.rm = TRUE))
     stop("truncnorm functions require that a <= b.")
-}
-
-logscale_sub = function(logx, logy) {
-  # In rare cases, logx can become numerically less than logy. When this
-  #   occurs, logx is adjusted and a warning is issued.
-  diff = logx - logy
-  if (any(diff < 0, na.rm = TRUE)) {
-    bad.idx = (diff < 0)
-    bad.idx[is.na(bad.idx)] = FALSE
-    logx[bad.idx] = logy[bad.idx]
-    warning("logscale_sub encountered negative value(s) of logx - logy (min: ",
-            formatC(min(diff[bad.idx]), format = "e", digits = 2), ")")
-  }
-  
-  scale.by = logx
-  scale.by[is.infinite(scale.by)] = 0
-  return(log(exp(logx - scale.by) - exp(logy - scale.by)) + scale.by)
 }
