2dd7ebca5342929bd2a5b603cc9f4c042f6ff59d
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-08-21 09:12:26 -0500

    Fixing this issue: You have file 'ashr/man/ashr.Rd' with \docType{package}, likely intended as a package overview help file, but without the appropriate PKGNAME-package \alias as per "Documenting packages" in R-exts.

--- R/ashr-package.R
+++ R/ashr-package.R
@@ -2,4 +2,5 @@
 #' @description The main function in the ashr package is \code{\link{ash}}, which should be examined for more details. For simplicity only the most commonly-used options are documented under \code{\link{ash}}. For expert or interested users the documentation for function \code{\link{ash.workhorse}} provides documentation on all implemented options.
 #' @name ashr
 #' @docType package
+#' @aliases ashr-package
 NULL
